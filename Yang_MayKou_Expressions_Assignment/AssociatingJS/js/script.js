2928c8eb852f7de3bb75a94f5e160b96e2cd6f3e
mayk789 <mayk789@gmail.com>
2015-02-12 23:09:41 -0600

    Cleared name values in variables
    Cleared the name values in variable amountInSales & variable
    howMuchSpent.

--- Yang_MayKou_Expressions_Assignment/AssociatingJS/js/script.js
+++ Yang_MayKou_Expressions_Assignment/AssociatingJS/js/script.js
@@ -8,17 +8,26 @@
 //alert("Javascript Test");
 
 //Variables and Data Types
-var startingAmount = ["100", "200", "300", "400"];
-var amountInSales = ["100", "200", "300", "400", "500", "600", "700", "800", "900"];
-var howMuchSpent = ["0","100","200", "300", "400", "500", "600", "700", "800", "900"]
+var startingAmount = [100, 200, 300, 400, 500];
+var amountInSales = [];
+var howMuchSpent = [];
+var endOfDay = howMuchSpent + amountInSales; // adds startingAmount and amountInSales
+
+
+
 
 //Prompts
-startingAmount = prompt("Enter today's starting amount: ", "100");
-amountInSales = prompt("Enter today's amount in sales. Please round to the nearest hundred: ")
-howMuchSpent = prompt("Enter the amount spent today. Please round to the nearest hundred: ")
+startingAmount = prompt("Enter today's starting amount. Please round to the nearest hundred.: ");
+amountInSales = prompt("Enter today's amount in sales. Please round to the nearest hundred.: ")
+howMuchSpent = prompt("Enter the amount spent today. Please round to the nearest hundred.: ")
 
 
 //Outputs
 console.log("Your starting amount today is: " + startingAmount);
 console.log("Your amount in sales today (rounded to the nearest hundred): " + amountInSales)
 console.log("Total amount you spent today (rounded to the nearest hundred: " + howMuchSpent)
+
+
+
+
+
