8b925f113abca98c82125a4fd0576c21f93ab126
mayk789 <mayk789@gmail.com>
2015-02-13 02:23:01 -0600

    Set amounts for variables
    Created set amounts for variables.

--- Yang_MayKou_Expressions_Assignment/AssociatingJS/js/script.js
+++ Yang_MayKou_Expressions_Assignment/AssociatingJS/js/script.js
@@ -8,26 +8,24 @@
 //alert("Javascript Test");
 
 //Variables and Data Types
-var startingAmount = [100, 200, 300, 400, 500];
-var amountInSales = [];
-var howMuchSpent = [];
-var endOfDay = howMuchSpent + amountInSales; // adds startingAmount and amountInSales
-
+var startingAmount = [100];
+var amountInSales = [400];
+var howMuchSpent = [10];
+var endOfDay = (amountInSales - howMuchSpent);
 
 
 
 //Prompts
-startingAmount = prompt("Enter today's starting amount. Please round to the nearest hundred.: ");
-amountInSales = prompt("Enter today's amount in sales. Please round to the nearest hundred.: ")
-howMuchSpent = prompt("Enter the amount spent today. Please round to the nearest hundred.: ")
+startingAmount = prompt("Enter today's starting amount which is $100.Simply enter 100.: ", "100");
+amountInSales = prompt("Made a total of $400 in sales. Please enter 400.: ", "400")
+howMuchSpent = prompt("$10 was spent on lunch. Please enter in 10.: ", "10")
 
 
 //Outputs
 console.log("Your starting amount today is: " + startingAmount);
 console.log("Your amount in sales today (rounded to the nearest hundred): " + amountInSales)
 console.log("Total amount you spent today (rounded to the nearest hundred: " + howMuchSpent)
-
-
+console.log("Your end of day total is: " + endOfDay)
 
 
 
