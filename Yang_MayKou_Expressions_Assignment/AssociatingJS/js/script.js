8d91027472a087c90bb483dd430f0dabf16cc168
mayk789 <mayk789@gmail.com>
2015-02-13 16:34:30 -0600

    Added more variables & added arithmetic operators
    Created more variables and added addition and subtraction operators.

--- Yang_MayKou_Expressions_Assignment/AssociatingJS/js/script.js
+++ Yang_MayKou_Expressions_Assignment/AssociatingJS/js/script.js
@@ -10,15 +10,16 @@
 //Variables and Data Types
 var startingAmount = [100];
 var amountInSales = [400];
-var howMuchSpent = [10];
-var endOfDay = (amountInSales - howMuchSpent);
-
-
+var howMuchSpent = [3, 5];
+var endOfDay;
+endOfDay = (100 + 400) - howMuchSpent;
+var actualProfit;
+actualProfit = 400 - (100 + howMuchSpent)
 
 //Prompts
-startingAmount = prompt("Enter today's starting amount which is $100.Simply enter 100.: ", "100");
-amountInSales = prompt("Made a total of $400 in sales. Please enter 400.: ", "400")
-howMuchSpent = prompt("$10 was spent on lunch. Please enter in 10.: ", "10")
+startingAmount = prompt("Enter today's starting amount which is $100.Simply enter 100. : ", "100");
+amountInSales = prompt("Made a total of $400 in sales. Please enter 400. : ", "400")
+howMuchSpent = prompt("$8 was spent on lunch. Please enter in 8. : ")
 
 
 //Outputs
@@ -26,6 +27,5 @@ console.log("Your starting amount today is: " + startingAmount);
 console.log("Your amount in sales today (rounded to the nearest hundred): " + amountInSales)
 console.log("Total amount you spent today (rounded to the nearest hundred: " + howMuchSpent)
 console.log("Your end of day total is: " + endOfDay)
-
-
+console.log("Your actual profit today is: " + actualProfit)
 
