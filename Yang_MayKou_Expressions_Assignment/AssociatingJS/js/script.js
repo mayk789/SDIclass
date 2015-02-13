bbf24feac897fcac657dd6c670dc22959fb94c66
mayk789 <mayk789@gmail.com>
2015-02-12 20:56:53 -0600

    Created variable, prompt, output
    Create startingAmount variable, created prompt to enter in this
    variable and also the output which shows what has been entered as the
    startingAmount variable.

--- Yang_MayKou_Expressions_Assignment/AssociatingJS/js/script.js
+++ Yang_MayKou_Expressions_Assignment/AssociatingJS/js/script.js
@@ -1,7 +1,19 @@
 //May Kou Yang
+//Feb. 12, 2015
 //SDI 2330
-//Section 01
+//Section 02
 //Week2_Expression_Assignment
 
 //Adding alert to test js file
-alert("Javascript Test")
+//alert("Javascript Test");
+
+//Variables and Data Types
+var startingAmount;
+
+
+//Prompts
+startingAmount = prompt("Enter your starting amount: ");
+
+
+//Outputs
+console.log("Starting amount entered: " + startingAmount);
