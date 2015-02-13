9b192e356cae1878c9cebd7fe34c11415f17cd07
mayk789 <mayk789@gmail.com>
2015-02-13 17:04:44 -0600

    Added arrays
    Added an example of array in the variable howMuchSpent.

--- Yang_MayKou_Expressions_Assignment/AssociatingJS/js/script.js
+++ Yang_MayKou_Expressions_Assignment/AssociatingJS/js/script.js
@@ -12,20 +12,20 @@ var startingAmount = [100];
 var amountInSales = [400];
 var howMuchSpent = [3, 5];
 var endOfDay;
-endOfDay = (100 + 400) - howMuchSpent;
+endOfDay = (startingAmount[0] + amountInSales[0]) - (howMuchSpent[0] + howMuchSpent[1]);
 var actualProfit;
-actualProfit = 400 - (100 + howMuchSpent)
+actualProfit = amountInSales[0] - (startingAmount[0] + (howMuchSpent[0] + howMuchSpent[1]));
 
 //Prompts
 startingAmount = prompt("Enter today's starting amount which is $100.Simply enter 100. : ", "100");
-amountInSales = prompt("Made a total of $400 in sales. Please enter 400. : ", "400")
-howMuchSpent = prompt("$8 was spent on lunch. Please enter in 8. : ")
+amountInSales = prompt("Made a total of $400 in sales. Please enter 400. : ", "400");
+howMuchSpent = prompt("$8 was spent on lunch. Please enter in 8. : ");
 
 
 //Outputs
 console.log("Your starting amount today is: " + startingAmount);
-console.log("Your amount in sales today (rounded to the nearest hundred): " + amountInSales)
-console.log("Total amount you spent today (rounded to the nearest hundred: " + howMuchSpent)
-console.log("Your end of day total is: " + endOfDay)
-console.log("Your actual profit today is: " + actualProfit)
+console.log("Your amount in sales today (rounded to the nearest hundred): " + amountInSales);
+console.log("Total amount you spent today (rounded to the nearest hundred: " + howMuchSpent);
+console.log("Your end of day total is: " + endOfDay);
+console.log("Your actual profit today is: " + actualProfit);
 
