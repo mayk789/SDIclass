24602b73f50b0a9ae590a7edb8f73a1559da1c73
mayk789 <mayk789@gmail.com>
2015-02-19 20:52:32 -0600

    Added outputs and created more variables

--- Yang_MayKou_Conditionals_Assignment/js/script.js
+++ Yang_MayKou_Conditionals_Assignment/js/script.js
@@ -10,8 +10,15 @@ Conditionals Assignment
 
 //Variables
 var farmLocation = "Afton";
-var acreage = "10 Acres";
+var acreage = "10";
+var lake = "1";
+var usableLand;
+    usableLand = acreage - lake;
 var rainFlood = "false";
+var farmable = "true";
 
-
-
+//Outputs
+console.log("The farm is located in " + farmLocation + ",MN.");
+console.log("It has a total of " + acreage + " acres.");
+console.log("There's a lake that takes up " + lake + " acres.");
+console.log("Usable amount of land should be " + usableLand + ".")
