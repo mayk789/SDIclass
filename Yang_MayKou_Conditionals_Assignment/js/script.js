182bf037761f586f97bbaa357565df99a8a6de17
mayk789 <mayk789@gmail.com>
2015-02-19 22:11:06 -0600

    Corrections
    Made corrections, added more variables, prompt, !

--- Yang_MayKou_Conditionals_Assignment/js/script.js
+++ Yang_MayKou_Conditionals_Assignment/js/script.js
@@ -12,15 +12,17 @@ Conditionals Assignment
 var farmLocation = "Afton";
 var acreage = "10";
 var lake = "1";
+var woods = "1";
 var usableLand;
-    usableLand = acreage - lake;
+    usableLand = parseInt(acreage - (parseInt(lake) + parseInt(woods)));
 var rainFlood = "true";
-var farmable = "true";
+
 
 //Outputs
 console.log("The farm is located in " + farmLocation + ", MN.");
 console.log("It has a total of " + acreage + " acres.");
 console.log("There's a lake that takes up " + lake + " acres.");
+console.log("There is also " + woods + " acre of woods that is on the farm.");
 console.log("Usable amount of land should be " + usableLand + ".");
 console.log("It is " + rainFlood + " that rain had flooded the land.");
 
@@ -28,9 +30,17 @@ console.log("It is " + rainFlood + " that rain had flooded the land.");
 rainFlood = confirm("Did rain flood the land? Select \"OK\" for true & \"Cancel\" for false." );
 
 //Conditional
-if (rainFlood == true) {
-    console.log("Yes,the land has been flooded by rain.");
-}else{
+if (rainFlood != true) {
+    // If false
     console.log("No, the land has not been flooded by rain.");
 
+} else {
+    // If true
+    var landFlooded = prompt("How many acres have been flooded? Please use whole numbers.", "8");
+    landFlooded = parseInt(landFlooded)
+        console.log("Here is the amount of land that has been flooded: " + landFlooded + " acre(s).");
+    var amountFarmable;
+    amountFarmable = parseInt(usableLand) - parseInt(landFlooded)
+        console.log("The actual amount of land farmable is: " + amountFarmable + " acres(s).")
 }
+
