c7e96d86cc7b6ca9cf6d7b73c6268b16a606e9f2
mayk789 <mayk789@gmail.com>
2015-02-19 19:50:30 -0600

    Variances
    Created some variances

--- Yang_MayKou_Conditionals_Assignment/js/script.js
+++ Yang_MayKou_Conditionals_Assignment/js/script.js
@@ -1,4 +1,17 @@
 /*
-SDI Conditionals Assignment
+Yang_MayKou
+SDI Section 02
+Week 3
+Conditionals Assignment
  */
- alert("Javascript is working");
+
+//alert("Javascript is working");
+
+
+//Variables
+var farmLocation = "Afton";
+var acreage = "10 Acres";
+var rainFlood = "false";
+
+
+
